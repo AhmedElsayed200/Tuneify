bb411f9d22a9bcaa8418070f922d4f6c0b22ea29
Ahmed Elsayed <66392241+AhmedElsayed200@users.noreply.github.com>
2024-08-14 14:48:26 +0200

    artist details page with his related songs

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,5 +1,40 @@
-const ArtistDetails = () => (
-  <div>ArtistDetails</div>
-);
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+import {
+  useGetArtistDetailsQuery,
+  useGetArtistSongsQuery,
+} from '../redux/APIs/libraryAPI';
+
+const ArtistDetails = () => {
+  const { artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const {
+    data: artistData,
+    isFetching,
+    error,
+  } = useGetArtistDetailsQuery(artistId);
+  const { data: artistSongs } = useGetArtistSongsQuery(artistId);
+
+  if (isFetching) return <Loader />;
+
+  if (error) return <Error />;
+
+  console.log(artistSongs);
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader artistId={artistId} artistData={artistData?.data[0]} />
+
+      <RelatedSongs
+        songs={artistSongs?.data}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+      />
+    </div>
+  );
+};
 
 export default ArtistDetails;
