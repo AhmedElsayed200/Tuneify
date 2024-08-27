9f6c043a4480b81ed5e7f7ac9d721e3386ce6c73
Ahmed Elsayed <66392241+AhmedElsayed200@users.noreply.github.com>
2024-08-27 20:46:40 +0200

    top charts page + updates on discover page

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { genres } from '../assets/constants';
+import { useSelector } from 'react-redux';
 import { useGetSongsByGenreQuery } from '../redux/APIs/libraryAPI';
 
 import SongCard from '../components/SongCard';
@@ -9,12 +7,14 @@ import Error from '../components/Error';
 
 const Discover = () => {
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const dispatch = useDispatch();
 
   console.log(activeSong);
 
-  const [selectedGenre, setSelectedGenre] = useState();
-  const { data: songs, isFetching, error } = useGetSongsByGenreQuery();
+  const {
+    data: songs,
+    isFetching,
+    error,
+  } = useGetSongsByGenreQuery('293401556');
 
   if (isFetching) return <Loader />;
   if (error) return <Error />;
@@ -24,20 +24,7 @@ const Discover = () => {
   return (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center flex-col mt-4 mb-10 sm:flex-row">
-        <h2 className="text-white font-bold text-3xl">
-          Discover {selectedGenre}
-        </h2>
-        <select
-          className="bg-black text-white text-xs rounded-lg w-[100px] p-2 focus:border-none focus:outline-none"
-          onChange={(e) => setSelectedGenre(e.target.value)}
-          value={selectedGenre}
-        >
-          {genres.map((genre) => (
-            <option key={genre.title} value={genre.value}>
-              {genre.title}
-            </option>
-          ))}
-        </select>
+        <h2 className="text-white font-bold text-3xl">Discover</h2>
       </div>
 
       <div className="flex flex-wrap justify-center gap-10 sm:justify-start">
--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,42 @@
-const TopCharts = () => <div>TopCharts</div>;
+import { useSelector } from 'react-redux';
+import { useGetSongsByGenreQuery } from '../redux/APIs/libraryAPI';
+
+import SongCard from '../components/SongCard';
+import Loader from '../components/Loader';
+import Error from '../components/Error';
+
+const TopCharts = () => {
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  const {
+    data: songs,
+    isFetching,
+    error,
+  } = useGetSongsByGenreQuery('109582281');
+
+  if (isFetching) return <Loader />;
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center flex-col mt-4 mb-10 sm:flex-row">
+        <h2 className="text-white font-bold text-3xl">Top Charts</h2>
+      </div>
+
+      <div className="flex flex-wrap justify-center gap-10 sm:justify-start">
+        {songs?.tracks?.map((song, i) => (
+          <SongCard
+            key={song.key}
+            song={song}
+            index={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            songs={songs?.tracks}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TopCharts;
